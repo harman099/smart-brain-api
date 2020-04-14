0c45a9a672bb221e0a32b1c3985ffb61ad8786ea
harman099 <61110096+harman099@users.noreply.github.com>
2020-04-14 17:18:53 +0530

    Adding port

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.get('/profile/:id', (req,res) => {profile.handleProfile(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req,res,db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req,res)})
 
-app.listen(3000, ()=>{
-	console.log('app running on port 3000');
+const PORT = process.env.PORT;
+app.listen(PORT || 3000, ()=>{
+	console.log(`APP IS RUNNING ON PORT ${PORT}`);
 })
